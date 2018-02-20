636b060833ae1d9f8c9459018807c439989a0e80
Jose Ramirez <joramirez@tesla.com>
2018-02-20 08:01:47 -0800

    refactored passport and authRoutes

--- index.js
+++ index.js
@@ -1,35 +1,8 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys');
+require('./services/passport')
+const authRoutes = require('./routes/authRoutes');
 const app = express();
-
-passport.use(
-    new GoogleStrategy({
-        clientID : keys.googleClientID,
-        clientSecret : keys.googleClientSecret,
-        callbackURL: 'google/callback'
-        }, 
-        (accessToken, refreshToken, profile, done) => {
-            console.log(accessToken);
-            console.log(refreshToken);
-            console.log(profile);
-        }
-    )
-);
-
-app.get(
-    '/auth/google/callback',
-    passport.authenticate('google')
-);
-
-app.get(
-    '/auth/google', 
-    passport.authenticate('google', {
-        scope : ['profile','email']
-    })
-);
+authRoutes(app);
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT);
