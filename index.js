48d7b0405e419c377fc379b89b4580d344650f7f
Jose Ramirez <joramirez@tesla.com>
2018-02-20 07:42:20 -0800

    added nodemon

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
 const express = require('express');
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy();
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('./config/keys');
 const app = express();
 
-passport.use(new GoogleStrategy({
-    clientID : keys.googleClientID,
-    clientSecret : keys.googleClientSecret,
-    callbackURL: 'auth/google/callback'
-}, accessToken => {
-    console.log(accessToken);
-} ));
+passport.use(
+    new GoogleStrategy({
+        clientID : keys.googleClientID,
+        clientSecret : keys.googleClientSecret,
+        callbackURL: 'google/callback'
+        }, 
+        (accessToken, refreshToken, profile, done) => {
+            console.log(accessToken);
+            console.log(refreshToken);
+            console.log(profile);
+        }
+    )
+);
 
+app.get(
+    '/auth/google/callback',
+    passport.authenticate('google')
+);
+
+app.get(
+    '/auth/google', 
+    passport.authenticate('google', {
+        scope : ['profile','email']
+    })
+);
 
 const PORT = process.env.PORT || 5000;
-
-console.log('running on port: ' + PORT)
 
 app.listen(PORT);
