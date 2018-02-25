263f3bcdbb48dd033ecd9983fca3cca7160d5c97
Jose Ramirez <joramirez@tesla.com>
2018-02-24 16:24:48 -0800

    add base url setting

--- services/passport.js
+++ services/passport.js
@@ -20,7 +20,7 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback'
+      callbackURL: keys.baseURL + '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
       User.findOne({ googleId: profile.id }).then(existingUser => {
